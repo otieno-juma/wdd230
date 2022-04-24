b62a8b96245dbd0d036e439a1f3e1c7f222ac62e
otieno-juma <oti22001@byui.edu>
2022-04-24 19:21:57 +0300

    update on the repo

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,4 +1,7 @@
+  // current year
+  const options = {year: 'numeric'};
+  document.queryselector('year').textcontent = new year().toLocaleDateString('en', options);
   // current date 
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
-  document.getElementById('currentdate').textcontent = new Date().tolocaleDatestring('en-US', options);
+  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  document.getElementById('currentdate').textcontent = new Date().toLocaleDateString('en', options);
   
