22a20a20ebb19898460e93d505a43038ba15504a
otieno-juma <103896399+otieno-juma@users.noreply.github.com>
2022-09-26 21:05:48 +0300

    update

---
+++ lesson1/scripts/currentdate.js
@@ -0,0 +1,10 @@
+const options = {
+    year: "numeric",
+  };
+  document.getElementById("current_year").textContent =
+    new Date().toLocaleDateString("en-US", options);
+
+//TO GET THE LAST MOODIFIED TIME USING JS
+// alert(document.lastModified);
+let oLastModif = (document.lastModified);
+document.getElementById("last_update").innerHTML = oLastModif;
