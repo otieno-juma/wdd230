4b6c8c1538404f82fcd04b75cec91b94130a8cca
otieno-juma <103896399+otieno-juma@users.noreply.github.com>
2022-11-08 19:42:38 +0300

    update

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -22,8 +22,42 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 // the last modified date at footer
 document.querySelector("#lastModified").textContent =  `Last Modification: ${document.lastModified}`;
 
+date =  new Date()
+const dayOfWeek = date.getDay()
+
+let message;
+
+if (dayOfWeek >= 1  && dayOfWeek <=2) {
+    message = "🤝 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}else{
+    message = ''
+}
+document.querySelector('#meeting').textContent = message
+document.querySelector('#small-meeting').textContent = message
+
+if ((day === 1) || (day === 2)) {banner.style.display = "block"}
+else {banner.style.display = "none"}
 const banner = document.querySelector("#banner");
 let day = date.getDay();
 
 if ((day === 1) || (day === 2)) {banner.style.display = "block"}
 else {banner.style.display = "none"}
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
