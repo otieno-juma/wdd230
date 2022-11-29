4c8366ec77e008caf3242c9e6a831553bdc43996
otieno-juma <103896399+otieno-juma@users.noreply.github.com>
2022-11-29 17:49:39 +0300

    update

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -32,6 +32,7 @@ if (dayOfWeek >= 1  && dayOfWeek <=2) {
 }else{
     message = ''
 }
+
 document.querySelector('#meeting').textContent = message
 document.querySelector('#small-meeting').textContent = message
 
@@ -39,25 +40,24 @@ if ((day === 1) || (day === 2)) {banner.style.display = "block"}
 else {banner.style.display = "none"}
 const banner = document.querySelector("#banner");
 let day = date.getDay();
-
 if ((day === 1) || (day === 2)) {banner.style.display = "block"}
 else {banner.style.display = "none"}
-const todayDisplay = document.querySelector(".today");
+
+
+// const todayDisplay = document.querySelector(".today");
 const visitsDisplay = document.querySelector(".visits");
 
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
 // determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
+if (!numVisits) {
 	visitsDisplay.textContent = `This is your first visit!`;
+} else {
+    visitsDisplay.textContent = `you have visited the site ${numVisits} times.`;
 }
 
 // increment the number of visits.
 numVisits++;
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
-// show todays date.
-todayDisplay.textContent = Date.now();
